7fa3df161efaa75c3e86e97e6b112755f730c6fd
Claus Jørgensen <clausjoergensen91@gmail.com>
2016-04-04 10:27:09 +0200

    created all classes in modellayer

---
+++ Back-end/BackEnd4Semester/Model/ContentInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class ContentInfo
+    {
+        public string Title { get; set; }
+        public Boolean IsPublic { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ Back-end/BackEnd4Semester/Model/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Member : User
+    {
+        public Boolean CanEdidt { get; set; }
+    }
+}
