8aacfb855edfa95b28ffc174dadabd0ba4327777
Peter Mogensen <rothmann27@gmail.com>
2016-04-04 13:07:53 +0200

    Servicecontracts

--- Back-end/BackEnd4Semester/Model/Player.cs
+++ Back-end/BackEnd4Semester/Model/Player.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Player : Member
+    public class Player : Member
     {
         public int Number { get; set; }
         public int GamesPlayed { get; set; }
---
+++ Back-end/BackEnd4Semester/Service/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Model;
+
+namespace Service
+{
+    [ServiceBehavior(ConcurrencyMode=ConcurrencyMode.Multiple)]
+    public class Service : IService 
+    {
+        Player getPlayer(String email)
+        {
+            return new Player();
+        }
+    }
+}
