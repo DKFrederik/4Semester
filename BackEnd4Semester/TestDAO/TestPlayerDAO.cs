306fa54032fb14735161a190e26715dd4b00924c
Frederik <Frederik@FREDERIK-LAPTOP>
2016-04-18 20:25:45 +0200

    test

--- BackEnd4Semester/TestDAO/TestPlayerDAO.cs
+++ BackEnd4Semester/TestDAO/TestPlayerDAO.cs
@@ -12,12 +12,12 @@ namespace TestDAO
     [TestClass]
     public class TestPlayerDAO
     {
-        private PlayerDao pDao;
+        private PlayerDao pdao;
 
         [TestInitialize]
         public void Initialize()
         {
-            pDao = new PlayerDao();
+            pdao = new PlayerDao();
         }
 
         [TestMethod]
@@ -25,7 +25,7 @@ namespace TestDAO
         {
             string firstname = "testFindPlayer";
             string lastname = "testFindPlayer";
-            Assert.IsNotNull(pDao.FindPlayer(firstname, lastname));
+            Assert.IsNotNull(pdao.FindPlayer(firstname, lastname));
         }
         [TestMethod]
         public void TestCreatePlayer()
@@ -45,7 +45,7 @@ namespace TestDAO
             };
 
 
-            Assert.AreNotEqual(-1, (player));
+            Assert.AreNotEqual(-1, pdao.CreatePlayer(player));
         }
 
         [TestMethod]
@@ -68,7 +68,7 @@ namespace TestDAO
                 Penalties = 0
             };
 
-            Assert.AreNotEqual(-1, pDao.UpdatePlayer(player, oldFirstname, oldLastname));
+            Assert.AreNotEqual(-1, pdao.UpdatePlayer(player, oldFirstname, oldLastname));
         }
 
         [TestMethod]
@@ -76,7 +76,7 @@ namespace TestDAO
         {
             string email = "TestEmail";
 
-            Assert.AreNotEqual(-1, pDao.DeletePlayer(email));
+            Assert.AreNotEqual(-1, pdao.DeletePlayer(email));
         }
     }
 }
